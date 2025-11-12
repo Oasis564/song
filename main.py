e54311aad813951d7458f09b1f79175cea219522
osaeid rony <osaeidfire@gmail.com>
2025-11-12 18:20:27 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def opening_function():
+    f = open("Believer.txt", 'r')
+    data = f.read()
+    print(data)
+    f.close()
+
+def char
+
+if __name__ == "__main__":
+    opening_function()
+    # characters_function()
+    # word_function()
+    # lowerletter_function()
+    # Slines_function()
+    # no_words_function()
