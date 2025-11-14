d6c2a129c601d9a2fd1b6f98b108c88dc37114a1
osaeid rony <osaeidfire@gmail.com>
2025-11-14 18:20:47 +0000

    song

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ def opening_function():
     print(data)
     f.close()
 
-def char
+def characters_function():
+    f = open("Believer.txt", 'r')
+    data = f.read()
+    length = len(data)
+    print(length)
+    
+def word_function():
+    f = open("Believer.txt", 'r')
+    data = f.read()
+    split = data.split()
+    print(len(split))
 
 if __name__ == "__main__":
     opening_function()
-    # characters_function()
-    # word_function()
+    characters_function()
+    word_function()
     # lowerletter_function()
     # Slines_function()
     # no_words_function()
+    
