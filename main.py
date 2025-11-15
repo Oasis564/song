9fb3e147e4d1a67e4a9cd4e242c138c929354656
osaeid rony <osaeidfire@gmail.com>
2025-11-15 16:23:31 +0000

    2nd

--- main.py
+++ main.py
@@ -15,12 +15,38 @@ def word_function():
     data = f.read()
     split = data.split()
     print(len(split))
+    
+def lowerletter_function():
+    f = open("Believer.txt", 'r')
+    data = f.read()
+    print(data.lower())
+    
+def Slines_function():
+    """
+    step 1:
+        read all the lines of a file as a list and store it in a variable.
+    
+    step 2: 
+        loop through the list and check if the first letter is s.
+        
+    step 3:
+        store the number of s found inside a variable.
+        
+    step 4:
+        print the number of S's found.
+    """
+    f = open("Believer.txt", 'r')
+    data = f.readlines()
+    for i in range(72):
+        if data[i][0].lower() == "s":
+            print(data[i])
+ 
 
 if __name__ == "__main__":
-    opening_function()
-    characters_function()
-    word_function()
+    # opening_function()
+    # characters_function()
+    # word_function()
     # lowerletter_function()
-    # Slines_function()
+    Slines_function()
     # no_words_function()
     
